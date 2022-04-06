2d6e59ebf4e632b74391f598fa7e8e762dc13fd1
GehaoYan <92266009+Steve724@users.noreply.github.com>
2022-04-06 16:15:47 -0400

    version 2

--- app/src/main/java/com/animee/constellation/utils/URLContent.java
+++ app/src/main/java/com/animee/constellation/utils/URLContent.java
@@ -7,6 +7,84 @@ public class URLContent {
 
 //     match constellation interface
     public static String getParnterURL(String men,String woman){
+        switch (men){
+            case "Aries":
+                men = "白羊座";
+                break;
+            case "Taurus":
+                men = "金牛座";
+                break;
+            case "Gemini":
+                men = "双子座";
+                break;
+            case "Cancer":
+                men = "巨蟹座";
+                break;
+            case "Leo":
+                men = "狮子座";
+                break;
+            case "Virgo":
+                men = "处女座";
+                break;
+            case "Libra":
+                men = "天秤座";
+                break;
+            case "Scorpio":
+                men = "天蝎座";
+                break;
+            case "Sagittarius":
+                men = "射手座";
+                break;
+            case "Capricorn":
+                men = "摩羯座";
+                break;
+            case "Aquarius":
+                men = "水瓶座";
+                break;
+            case "Pisces":
+                men = "双鱼座";
+                break;
+            default:break;
+        }
+        switch (woman){
+            case "Aries":
+                woman = "白羊座";
+                break;
+            case "Taurus":
+                woman = "金牛座";
+                break;
+            case "Gemini":
+                woman = "双子座";
+                break;
+            case "Cancer":
+                woman = "巨蟹座";
+                break;
+            case "Leo":
+                woman = "狮子座";
+                break;
+            case "Virgo":
+                woman = "处女座";
+                break;
+            case "Libra":
+                woman = "天秤座";
+                break;
+            case "Scorpio":
+                woman = "天蝎座";
+                break;
+            case "Sagittarius":
+                woman = "射手座";
+                break;
+            case "Capricorn":
+                woman = "摩羯座";
+                break;
+            case "Aquarius":
+                woman = "水瓶座";
+                break;
+            case "Pisces":
+                woman = "双鱼座";
+                break;
+            default:break;
+        }
         men = men.replace("座","");
         woman = woman.replace("座","");
         try {
